72c346dbab63366cc7effd4cb62fa7f7c250d0c3
randreu28 <randreu28@gmail.com>
2022-09-17 19:06:07 +0200

    Changes design pattern
    As a previous step before step 2

--- components/PokemonRow.tsx
+++ components/PokemonRow.tsx
@@ -5,23 +5,25 @@ import type Pokemon from "../types/Pokemon";
 const PokemonRow: FC<Pokemon> = ({ id, name, sprite, types }) => {
   return (
     <>
-      <div className="space-y-3">
+      <div className="flex gap-5 p-5">
         <img
           className="w-64 h-64 bg-gray-200 rounded-lg p-5"
           alt=""
           src={sprite}
         />
-        <p className="text-gray-500">#{id.toString()}</p>
-        <h1 className="text-2xl">{name}</h1>
-        <div className="flex flex-row gap-2 flex-wrap max-w-xs">
-          {types.map((type) => (
-            <p
-              key={type}
-              className="rounded bg-gray-200 text-sm w-20 text-center px-3 py-1 mt-3"
-            >
-              {type}
-            </p>
-          ))}
+        <div className="flex flex-col">
+          <p className="text-gray-500">#{id.toString()}</p>
+          <h1 className="text-2xl">{name}</h1>
+          <div className="flex flex-row gap-2 flex-wrap max-w-xs">
+            {types.map((type) => (
+              <p
+                key={type}
+                className="rounded bg-gray-200 text-sm w-20 text-center px-3 py-1 mt-3"
+              >
+                {type}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
     </>
