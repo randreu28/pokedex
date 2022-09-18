2e64210d0645f74a1e69a11cdcf361c7e1aab20e
randreu28 <randreu28@gmail.com>
2022-09-18 18:10:35 +0200

    Adpots clearer type/interface syntax
    https://www.typescriptlang.org/docs/handbook/basic-types.html\#about-number-string-boolean-symbol-and-object

--- types/Pokemon.ts
+++ types/Pokemon.ts
@@ -1,11 +1,8 @@
 interface Pokemon {
-  id: Number;
+  id: number;
   name: string; 
-  types: Array<string>;
+  types: string[];
   sprite: string;
-  /* If I declare it as `String` (with caps), 
-    it throws an error when passing it to <image> src property.
-    Yet my IDE tells me it's right. Why? */
 }
 
 export default Pokemon;
